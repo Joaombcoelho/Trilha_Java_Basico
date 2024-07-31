560c17f519301e0f2fc5176cc332470374eb1352
JoaombCoelho <jaomanel6@gmail.com>
2024-07-30 23:03:33 -0300

    Update caixaEletronico

--- ControledeFluxo/src/CaixaEletronico.java
+++ ControledeFluxo/src/CaixaEletronico.java
@@ -1,5 +1,14 @@
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double saldo = 25.0;
+        double valorSolicitado = 10.0;
+
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo Saldo " + "R$ " + saldo);
+
+        }else
+        System.out.println("Saldo Insuficiente");
+        
     }
 }
