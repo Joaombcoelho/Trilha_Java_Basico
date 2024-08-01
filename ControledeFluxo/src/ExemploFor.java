ade270c7a422b8b6b14c9e006d6ae56b6ea42c55
JoaombCoelho <jaomanel6@gmail.com>
2024-08-01 20:11:23 -0300

    ExemploFor

---
+++ ControledeFluxo/src/ExemploFor.java
@@ -0,0 +1,6 @@
+public class ExemploFor
+    public static void main (string[] args) {
+    for (int carnerinhos = 0; carnerinhos =<20; carnerinhos ++ ){
+        System.out.println("contando Carneirinhos" + carneirinhos );
+    }
+}
